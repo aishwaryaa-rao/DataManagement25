3e50a518bc95392424b3d3852b6637ffded04ce3
Aishwarya Rao <aishwarya23.rao@gmail.com>
2024-03-19 14:08:57 +0000

    upd

--- R/transformation1.R
+++ R/transformation1.R
@@ -10,6 +10,7 @@ library(tidyr)
 library(ggplot2)
 
 # DB connection
+db_file_path <- "script/e-commerce.db"
 my_db <- RSQLite::dbConnect(RSQLite::SQLite(),db_file_path)
 
 # Create Tables in DB
@@ -310,7 +311,6 @@ id_is_duplicate_category <- data.frame(ifelse(id_duplicates_category, TRUE, FALS
 # Append Data into Database
 
 # db connection
-db_file_path <- "script/e-commerce.db"
 my_db <- RSQLite::dbConnect(RSQLite::SQLite(), db_file_path)
 
 # Write to customer
